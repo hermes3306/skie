e83503be2d85f88620907aa3716552ce9c4a0f71
hermes3306 <jason.park@altibase.com>
2023-11-23 10:08:50 +0900

    x

--- skie/3.reputation2.sh
+++ skie/3.reputation2.sh
@@ -1,1 +1,1 @@
-cat 3.reputation2.cypher | cypher-shell -u neo4j -p re91na00 -a neo4j://localhost:7689 -d neo4j
+cat 3.reputation2.cypher | cypher-shell -u neo4j -p re91na00 -a neo4j://localhost:7687 -d neo4j
