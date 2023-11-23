3267682dc7b8ca6726d79300b23a4fb1a1ef2058
hermes3306 <jason.park@altibase.com>
2023-11-23 17:37:24 +0900

    pc

--- skie/0.portchange.sh
+++ skie/0.portchange.sh
@@ -1,3 +1,4 @@
-find . -type f -name "*.sh" -exec sed -i 's/7489/7487/g' {} +
-#find . -type f -name "*.sh" -exec sed -i 's/7487/7489/g' {} +
+find . -type f -name "*.sh" ! -name "0.portchange.sh" -exec sed -i 's/7687/7689/g' {} +
+
+#find . -type f -name "*.sh" ! -name "0.portchange.sh" -exec sed -i 's/7689/7687/g' {} +
 
