cf63ef869e4de57db46a2e6e9a8a79ee544a0d3f
hermes3306 <bstyfs23@hotmail.com>
2023-11-26 13:23:20 +0000

    x

---
+++ python/csv_exp.py
@@ -0,0 +1,33 @@
+import os
+from neo4j import GraphDatabase
+
+
+# Neo4j connection details
+uri = "neo4j+s://942801e8.databases.neo4j.io:7687"
+username = "neo4j"
+password = "re91na00"
+
+# Cypher query to export data to CSV, change the query as needed
+cypher_query = "MATCH (n) RETURN n"
+
+# Output CSV file path
+output_csv_file = "output.csv"
+
+# Connect to Neo4j
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+# Run Cypher query to export data to CSV
+with driver.session() as session:
+    result = session.run(cypher_query)
+    with open(output_csv_file, "w", newline="", encoding="utf-8") as csvfile:
+        csvfile.write("node\n")  # Writing header if needed
+        for record in result:
+            csvfile.write(f"{record['n']}\n")  # Writing data to CSV
+
+# Close the connection
+driver.close()
+
+if os.path.exists(output_csv_file):
+    print(f"CSV file '{output_csv_file}' downloaded successfully.")
+else:
+    print("CSV file download failed.")
---
+++ python/csv_imp.py
@@ -0,0 +1,29 @@
+import os
+from neo4j import GraphDatabase
+
+
+# Neo4j connection details
+uri = "neo4j+s://942801e8.databases.neo4j.io:7687"
+username = "neo4j"
+password = "re91na00"
+
+# Cypher query to load data from CSV to Neo4j
+cypher_query = """
+USING PERIODIC COMMIT
+LOAD CSV WITH HEADERS FROM 'file:///$csv_file' AS row
+CREATE (:Node {node: row.node})
+"""
+
+# Output CSV file path
+output_csv_file = "output.csv"
+
+# Connect to Neo4j
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+# Run Cypher query to upload data from CSV to Neo4j
+with driver.session() as session:
+    result = session.run(cypher_query)
+    print("CSV data uploaded to Neo4j.")
+
+# Close the connection
+driver.close()
