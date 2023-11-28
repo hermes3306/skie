50baee5295515b353517dcf5dc671879fdcfdbee
hermes3306 <bstyfs23@hotmail.com>
2023-11-28 11:12:10 +0900

    passwd neo4j2012

--- python/ex.py
+++ python/ex.py
@@ -4,7 +4,7 @@ from neo4j import GraphDatabase
 # Connect to Neo4j, replace <uri>, <username>, and <password> with your credentials
 uri = "neo4j+s://942801e8.databases.neo4j.io:7687"
 username = "neo4j"
-password = "re91na00"
+password = "neo4j2022"
 
 # Establish a connection
 driver = GraphDatabase.driver(uri, auth=(username, password))
@@ -13,9 +13,13 @@ driver = GraphDatabase.driver(uri, auth=(username, password))
 def create_node(tx, name):
     tx.run("CREATE (:Person {name: $name})", name=name)
 
+def change_password(tx, pwd):
+    tx.run("ALTER USER neo4j set password $pwd", pwd=pwd)
+
 # Run the function within a Neo4j session
 with driver.session() as session:
     session.execute_write(create_node, "Alice")
+    #session.execute_write(change_password, "neo4j2022")
 
 # Define a function to read nodes
 def read_nodes(tx):
