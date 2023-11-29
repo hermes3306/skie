c6ae859db9b76ba15f621689eb5da8b4a702842d
hermes3306 <bstyfs23@hotmail.com>
2023-11-29 19:26:38 +0900

    x

--- aura/import_local.sh
+++ aura/import_local.sh
@@ -288,4 +288,5 @@ EOF
 
 # Run the function to create the Cypher query file
 create_cypher_query_file
+echo cypher-shell -u $NEO4J_USER -p $NEO4J_PASSWORD -a $NEO4J_URI -f imp.cypher
 cypher-shell -u $NEO4J_USER -p $NEO4J_PASSWORD -a $NEO4J_URI -f imp.cypher
