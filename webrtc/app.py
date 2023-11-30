7661ea6d3aeda3b81228f7b2ded54b1bd65add0a
hermes3306 <jason.park@altibase.com>
2023-11-30 17:12:44 +0900

    x

---
+++ webrtc/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(data):
+    print('Message from {}: {}'.format(data['username'], data['message']))
+    socketio.emit('message', data)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
---
+++ webrtc/app2.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('video_call.html')
+
+@socketio.on('join')
+def handle_join(data):
+    socketio.emit('join', data)
+
+@socketio.on('offer')
+def handle_offer(data):
+    socketio.emit('offer', data)
+
+@socketio.on('answer')
+def handle_answer(data):
+    socketio.emit('answer', data)
+
+@socketio.on('ice-candidate')
+def handle_ice_candidate(data):
+    socketio.emit('ice-candidate', data)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+
